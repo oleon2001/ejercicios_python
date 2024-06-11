421b8ab8a8b976fa06ec39d0075951b8ff9eaf07
OswaldoLeon <oswaldoleon72@gmail.com>
2024-06-11 16:05:37 -0400

    primer commit

--- ejercico_grafica/ejercicio_2/main.py
+++ ejercico_grafica/ejercicio_2/main.py
@@ -1,28 +1,25 @@
+import read
+import utils
 import csv
 
-def leer_ruta(ruta_archivo):
-    with open(ruta_archivo, "r") as archivo_ventas:
-        lector = csv.reader(archivo_ventas, delimiter=',')
-        datos = [fila for fila in lector]
-        return datos
-
 def run():
-    ruta_archivo = 'ejercicio_2/data_ventas.csv'
-    datos = leer_ruta(ruta_archivo)
-    primera_columna = [fila[0] for fila in datos]
-    
-    str(mes_elegido).capitalize() = input("Elegi el mes que quieres ver o tipea 'todos': ")
-    
-    if mes_elegido == 'todos':
-        for fila in datos:
-            print(fila)
+    mes_elegido=input("elegi el mes que quieres ver o tipea todos:")
+    data=read.leer_ruta('ejercicio_2/data_ventas.csv')
+    primera_columna = [fila[0] for fila in data]
+    if mes_elegido == str(primera_columna):
+     mostrar_data=list(map(lambda x: x['Mes']==mes_elegido,data))
+     print(mostrar_data)
+    elif mes_elegido == 'todos':
+      for fila in data:
+         print(fila)
     else:
-        mostrar_data = [fila for fila in datos if fila[0] == mes_elegido]
-        if mostrar_data:
-            for fila in mostrar_data:
-                print(fila)
-        else:
-            print("No existe ese mes")
+      print("no existe ese mes")
+
+    
+
+
+
+
 
 if __name__ == '__main__':
-    run()
+   run()
