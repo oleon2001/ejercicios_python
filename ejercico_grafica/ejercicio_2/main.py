20db140be30c2d2800b66aee7a97ff2e1d008839
OswaldoLeon <oswaldoleon72@gmail.com>
2024-06-12 16:26:57 -0400

    grafica V0.1.3 funciona pero se puede añadir mas

--- ejercico_grafica/ejercicio_2/main.py
+++ ejercico_grafica/ejercicio_2/main.py
@@ -14,24 +14,32 @@ def leer_datos(ruta_archivo):
         return datos
 '''
 
+
 def run():
     ruta_archivo = "/home/oleon/Escritorio/ejercicios_python/ejercico_grafica/ejercicio_2/data_ventas.csv"
-    data = read.leer(ruta_archivo)
+    try:
+        data = read.leer(ruta_archivo)
+    except FileNotFoundError:
+        print("Error: El archivo CSV no existe")
+        return
+
     meses_elegidos = input("Elegí el mes que quieres ver (separa por comas si son varios) o tipea 'todos': ")
-    #meses_elegidos = [x.strip() for x in meses_elegidos.split(",")]
-    if str(meses_elegidos).capitalize() == 'Todos':
+    meses_elegidos = [x.strip().capitalize() for x in meses_elegidos.split(",")]
+
+    if meses_elegidos== ['Todos']:
         labels = [x['Mes'] for x in data]
         values = [int(x['Ventas']) for x in data]
         charts.generate_bar_chart(labels, values)
-        charts.generate_pie_chart(labels, values)    
-    elif str(meses_elegidos).capitalize() != 'Todos':
-        resultado = [x for x in data if x['Mes'] == meses_elegidos]
-        labels = [x['Mes'] for x in resultado]
-        values = [int(x['Ventas']) for x in resultado]
-        charts.generate_bar_chart(labels, values)
         charts.generate_pie_chart(labels, values)
     else:
-        print("No se encontraron datos para el mes seleccionado")
+        resultado = [x for x in data if x['Mes'] in meses_elegidos]
+        if resultado:
+            labels = [x['Mes'] for x in resultado]
+            values = [int(x['Ventas']) for x in resultado]
+            charts.generate_bar_chart(labels, values)
+            charts.generate_pie_chart(labels, values)
+        else:
+            print("No se encontraron datos para el mes seleccionado")
 
 if __name__ == '__main__':
     run()
